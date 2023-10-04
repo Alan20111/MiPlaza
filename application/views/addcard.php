5b0120fe663b7d34670e469c68cd0831b23c1e8a
Alan20111 <alannnicanor62@gmail.com>
2023-10-04 12:44:34 -0600

    avances pagina admin

--- application/views/addcard.php
+++ application/views/addcard.php
@@ -32,7 +32,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
     <div class="conteiner m-4 p-4 w-auto bg-light bg-opacity-75 rounded shadow  text-light"
         style="height: calc(100vh - 140px);">
         <div class="w-100 h-100 row">
-            <div class="col-6 bg-danger h-100 shadow rounded p-3">
+            <div class="col-6 bg-danger h-100 shadow rounded p-3 text-center">
                 <p class="fs-3 badge bg-light text-wrap text-danger">Formulario para agregar targetas de personal</p>
                 <div class="row">
                     <div class="col w-50 h-100">
@@ -46,31 +46,31 @@ defined('BASEPATH') or exit('No direct script access allowed');
                         <div class="mb-3 text-dark">
                             <label for="" class="form-label text-light fw-medium">Actividades del puesto de
                                 trabajo:</label>
-                            <div class="input-group mb-1">
+                            <div class="input-group mb-2">
                                 <span class="input-group-text" id="basic-addon1">Act1:</span>
                                 <input type="text"
                                     class="form-control focus-ring focus-ring-light rounded-0 border border-1 rounded-end"
                                     id="floatingInput" placeholder="">
                             </div>
-                            <div class="input-group mb-1">
+                            <div class="input-group mb-2">
                                 <span class="input-group-text" id="basic-addon1">Act2:</span>
                                 <input type="text"
                                     class="form-control focus-ring focus-ring-light rounded-0 border border-1 rounded-end"
                                     id="floatingInput" placeholder="">
                             </div>
-                            <div class="input-group mb-1">
+                            <div class="input-group mb-2">
                                 <span class="input-group-text" id="basic-addon1">Act3:</span>
                                 <input type="text"
                                     class="form-control focus-ring focus-ring-light rounded-0 border border-1 rounded-end"
                                     id="floatingInput" placeholder="">
                             </div>
-                            <div class="input-group mb-1">
+                            <div class="input-group mb-2">
                                 <span class="input-group-text" id="basic-addon1">Act4:</span>
                                 <input type="text"
                                     class="form-control focus-ring focus-ring-light rounded-0 border border-1 rounded-end"
                                     id="floatingInput" placeholder="">
                             </div>
-                            <div class="input-group mb-1">
+                            <div class="input-group mb-2">
                                 <span class="input-group-text" id="basic-addon1">Act5:</span>
                                 <input type="text"
                                     class="form-control focus-ring focus-ring-light rounded-0 border border-1 rounded-end"
@@ -80,25 +80,88 @@ defined('BASEPATH') or exit('No direct script access allowed');
                     </div>
                     <div class="col w-50 h-100">
                         <div class="m-0">
-                            <label for="tittle" class="form-label">Titulo principal:</label>
-                            <input type="text" class="form-control focus-ring focus-ring-light border border-0 rounded-1"
-                                id="tittle" placeholder="">
+                            <label for="tittle" class="form-label fw-medium">Titulo principal:</label>
+                            <input type="text"
+                                class="form-control focus-ring focus-ring-light border border-0 rounded-1" id="tittle"
+                                placeholder="">
                         </div>
                         <div class="mb-4">
-                            <label for="tittle-nav" class="form-label">Titulo en navegador:</label>
-                            <input type="text" class="form-control focus-ring focus-ring-light border border-0 rounded-1"
+                            <label for="tittle-nav" class="form-label fw-medium">Titulo en navegador:</label>
+                            <input type="text"
+                                class="form-control focus-ring focus-ring-light border border-0 rounded-1"
                                 id="tittle-nav" placeholder="">
                         </div>
                         <div class="mb-4">
-                            <label for="descrip" class="form-label">Example textarea</label>
-                            <textarea class="form-control rounded-1" id="descrip" rows="10"></textarea>
+                            <label for="descrip" class="form-label fw-medium">Example textarea</label>
+                            <textarea
+                                class="form-control rounded-1 focus-ring focus-ring-light border border-0 rounded-1"
+                                id="descrip" rows="10"></textarea>
+                        </div>
+                        <div class="mb-4 row px-5">
+                            <label for="exampleColorInput" class="form-label fw-medium">Color de fondo:</label>
+                            <input type="color"
+                                class="form-control form-control-color col-5 border border-0 rounded-0 rounded-start w-25 focus-ring focus-ring-light"
+                                id="exampleColorInput" value="#dc3545" title="Choose your color">
+                            <div class="bg-light col-6 w-75 rounded-end">
+                                <div class="row">
+                                    <div class="col-6 text-dark p-1 fw-medium">Principal</div>
+                                    <div class="col-6 text-dark p-1 fw-medium">Secudario</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row text-center">
+                            <button type="button" class="btn btn-outline-light col-5 m-auto fw-medium">Dark</button>
+                            <button type="button" class="btn btn-outline-light col-5 m-auto">Dark</button>
                         </div>
                     </div>
                 </div>
 
             </div>
-            <div class="col-6 h-100 bg-light">
-                .col-6
+            <div class="col-6 h-100 ps-5 pe-2">
+                <div class="row w-auto h-auto">
+                    <div class="col-6 p-0 z-2">
+                        <img src="<?= base_url() ?>public/img/referencia.jpg" class="w-100 rounded-start-pill shadow" alt="">
+                    </div>
+                    <div class="col-6 text-center bg-danger shadow  z-2">
+                        <p class="fs-5 badge bg-light text-wrap text-danger rounded-1 mt-3 mb-0 shadow z-1">Formulario
+                            para agregar targetas
+                            de
+                            personal
+                        </p>
+                        <p class="fs-6 fw-normal badge text-light text-wrap text-danger rounded-0 m-0 w-75 z-0">
+                            Descripcion larga
+                            Xxxxxx xxxxx
+                            xxxxxxxxxxx xxxxx xxxxx x xxx xxxxxxxxx xxxxxxx xxxxx xxxxxxxxx xxxxxxxxxx xxxxxxxx xxx
+                            xxxxxxxxxx
+                        </p>
+                    </div>
+                    <div class="d-flex align-items-end flex-column w-100 p-0">
+                        <div class="conteiner w-75 p-0">
+                            <button class="btn btn-outline-danger w-100 px-0 me-2 rounded-0" type="button"
+                                data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false"
+                                aria-controls="collapseExample">
+                                Mas. . .
+                            </button>
+                            <div class="collapse text-light bg-danger p-0 rounded-0" id="collapseExample">
+                                <div class="row  card-body">
+                                    <div class="col-6">
+                                        <ul class="list-group list-group-flush  bg-light text-danger">
+                                            Actividades:
+                                            <li class="list-group-item bg-danger text-light">An item</li>
+                                            <li class="list-group-item bg-danger text-light">A second item</li>
+                                            <li class="list-group-item bg-danger text-light">A third item</li>
+                                            <li class="list-group-item bg-danger text-light">A fourth item</li>
+                                            <li class="list-group-item bg-danger text-light">And a fifth one</li>
+                                        </ul>
+                                    </div>
+                                    <div class="col-6">q</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
             </div>
         </div>
     </div>
