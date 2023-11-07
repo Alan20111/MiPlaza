524828658d22557e25b9e953a2494f8d11ca4b5c
Alan20111 <alannnicanor62@gmail.com>
2023-11-07 09:10:59 -0600

    Update miplaza.sql

--- system/database/miplaza.sql
+++ system/database/miplaza.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 13-10-2023 a las 22:02:07
+-- Tiempo de generación: 07-11-2023 a las 16:10:35
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -30,9 +30,10 @@ SET time_zone = "+00:00";
 CREATE TABLE `card-nosotros` (
   `id` int(250) NOT NULL,
   `img` varchar(500) NOT NULL,
+  `imgName` varchar(50) NOT NULL,
   `navtittle` varchar(50) NOT NULL,
   `tittle` varchar(50) NOT NULL,
-  `descripcion` varchar(500) NOT NULL,
+  `descripcion` varchar(200) NOT NULL,
   `color` varchar(50) NOT NULL,
   `sombra` varchar(50) NOT NULL,
   `act1` varchar(50) NOT NULL,
@@ -46,14 +47,15 @@ CREATE TABLE `card-nosotros` (
 -- Volcado de datos para la tabla `card-nosotros`
 --
 
-INSERT INTO `card-nosotros` (`id`, `img`, `navtittle`, `tittle`, `descripcion`, `color`, `sombra`, `act1`, `act2`, `act3`, `act4`, `act5`) VALUES
-(1, 'public/img/db/65284b4e3864a_admin-nosotros.jpg', 'sdfds', 'sfd', 'fs', '#b9414d', '#870f1b', 'sasdf', 'sadf', 'sdf', 'sdfs', 'adf'),
-(2, 'public/img/db/65296eab7f88e_1. Reporte Semanal de Aprendizaje -1.jpg', 'llll', 'l', 'lllll', '#60292f', '#2e0000', 'l', 'l', 'l', 'l', 'l'),
-(3, 'public/img/db/6529745bc621b_referencia.jpg', 'sdfgsdff', 'fsdg', 'dfgfd', '#28b867', '#008635', 'asda', 'sdasd', 'asd', 'asd', 'fgfdg'),
-(4, 'public/img/db/652978afeda7c_miplaza.jpg', 'gfdgfd', 'dfg', 'gfdgfddf', '#fb182f', '#c90000', 'ffsdf', 'sdfsd', 'fsdf', 'sd', 'fsdfs'),
-(5, 'public/img/db/65297bf2bacdc_grapes.png', 'dsad', 'sadas', 'sadsadsa', '#a132a9', '#6f0077', 'dsa', 'dsa', 'das', 'd', 'dsadsad'),
-(6, 'public/img/db/6529856a26529_Imagen de WhatsApp 2023-10-13 a las 11.57.09_3a18deec.jpg', 'fg', 'fdhfg', 'gfd', '#7ba4db', '#4972a9', 'asd', 'adas', 'f', 'gfdg', 'dfgfd'),
-(7, 'public/img/db/6529863732a2b_melon.png', 'dfg', 'gfdg', 'dsfg', '#ffffff', '#cdcdcd', 'fds', 'fsdf', 'sdf', 'sdf', 'sdfsd');
+INSERT INTO `card-nosotros` (`id`, `img`, `imgName`, `navtittle`, `tittle`, `descripcion`, `color`, `sombra`, `act1`, `act2`, `act3`, `act4`, `act5`) VALUES
+(1, 'public/img/db/6543fff23e1a8_Imagen de WhatsApp 2023-10-25 a las 08.02.01_138e62e6.jpg', 'Imagen de WhatsApp 2023-10-25 a las 08.02.01_138e6', 'Gte. General', 'Gerente General', 'El Gerente General es el encargado de supervisar y coordinar todas las operaciones en la tienda, desde la gestión de personal hasta el mantenimiento de inventario y la relación con proveedores, con el', '#49b62b', '#7be85d', 'Supervisar la Apertura de la Tienda', 'Gestión de Personal y Uniformes', 'Mantenimiento del Inventario y Almacén', 'Gestión de Proveedores', 'Facturación y Ventas'),
+(2, 'public/img/db/6543f35340b0c_Imagen de WhatsApp 2023-10-25 a las 08.02.01_7cf1abb7.jpg', 'Imagen de WhatsApp 2023-10-25 a las 08.02.01_7cf1a', 'Gte. Ope.', 'Gerente de Operaciones', 'El Gerente de Operaciones se encuentra en una posición estratégica para garantizar que la empresa funcione de manera eficiente, cumpliendo con los estándares de operación y los compromisos financieros', '#e62d40', '#ff5f72', 'Supervisión de Puntualidad y Asistencia', 'Control de Asistencia', 'Checklist para Gerentes de Tienda', 'Supervisión de Uniformes y Orden', 'Gestión de Finanzas y Personal'),
+(4, 'public/img/db/65452dccd77cc_Imagen de WhatsApp 2023-10-25 a las 08.02.01_df62b154.jpg', 'Imagen de WhatsApp 2023-10-25 a las 08.02.01_df62b', 'Almacén', 'Encargado de Almacén', 'Estas actividades son esenciales para mantener la operación del almacén en orden, garantizar la seguridad del personal y cumplir con los estándares de calidad y eficiencia.', '#eab034', '#ffe266', 'Gestión de Asistencia y Puntualidad', 'Apertura y Seguridad del Almacén', 'Supervisión del Personal', 'Gestión de Inventario', 'Interacción con Proveedores y Clientes'),
+(5, 'public/img/db/65454500a20f2_tree (1).jpg', 'tree (1).jpg', 'Aux. Admin.', 'Auxiliar Administrativo', 'Estas actividades están diseñadas para mostrar nuestro compromiso con un excelente servicio al cliente y la protección de tus datos personales. Si tienes alguna pregunta o inquietud, nuestros auxiliar', '#a434ef', '#d666ff', 'Brindar un Servicio Cordial y Profesional', 'Garantizar una Experiencia de Atención al Cliente ', 'Manejar Información Confidencial de Manera Segura', 'Facilitar la Resolución de Problemas y Peticiones', 'Asegurar la Precisión y Cumplimiento de Documentac'),
+(6, 'public/img/db/6545471f545d2_miplazaAMP.jpg', 'miplazaAMP.jpg', 'Surtidor', 'Surtidor de Bodega', 'Nuestro surtidor se compromete a brindar una atención excepcional a nuestros clientes. Escucha activamente sus necesidades, busca soluciones rápidas y efectivas, y se asegura de que los clientes se si', '#e13344', '#ff6576', 'Eficiencia y Puntualidad en el Suministro', 'Asegurando la Seguridad de las Mercancías', 'Mantenimiento del Stock y Organización', 'Relación con Clientes y Proveedores', 'Solución de Problemas Ágil'),
+(7, 'public/img/db/65454956850d3_referencia.jpg', 'referencia.jpg', 'Cajero', 'Cajero', 'El puesto de cajero es un papel central en nuestra empresa, ya que implica una interacción directa con nuestros valiosos clientes en el punto de venta. Los cajeros desempeñan un papel crucial en brind', '#477ff0', '#79b1ff', 'Gestionan su de Asistencia y Puntualidad', 'Cumplir con el Uniforme e Identificación', 'Realizar Cortes Precisos y Dentro del Margen de To', 'Supervisar Estantes, Faltantes y Caducidades', 'Brindar un Servicio Cordial y Profesional'),
+(8, 'public/img/db/654549da85122_Imagen de WhatsApp 2023-10-25 a las 08.02.01_138e62e6.jpg', 'Imagen de WhatsApp 2023-10-25 a las 08.02.01_138e6', 'Cargador', 'Cargador', 'El cargador es un miembro fundamental de nuestro equipo, desempeñando un papel crucial en la logística y operaciones de nuestro almacén. Su principal responsabilidad es garantizar un flujo de trabajo ', '#df8f34', '#ffc166', 'Trasladar mercancía', 'Cumplir con el Uniforme e Identificación', 'Surtir de Manera Adecuada y Eficiente', 'Mantener Orden y Limpieza en el Almacén', 'Apoyar en las Actividades del Almacén'),
+(9, 'public/img/db/65454adc25c75_Imagen de WhatsApp 2023-10-25 a las 08.02.01_df62b154.jpg', 'Imagen de WhatsApp 2023-10-25 a las 08.02.01_df62b', 'Carnes F.', 'Carnes Frías', 'Carnes Frías requiere habilidades específicas en el manejo de alimentos, atención al cliente y responsabilidad en el manejo de productos frescos. Los empleados de Carnes Frías desempeñan un papel cruc', '#ea2e41', '#ff6073', 'Orden y Limpieza del Área de Carnes Frías', 'Mantener Productos en Stock', 'Mantener la Cámara en Orden', 'Atención al Cliente', 'Manejo de Productos Frescos');
 
 -- --------------------------------------------------------
 
@@ -64,7 +66,7 @@ INSERT INTO `card-nosotros` (`id`, `img`, `navtittle`, `tittle`, `descripcion`, 
 CREATE TABLE `usuarios` (
   `id` int(11) NOT NULL,
   `user` varchar(100) NOT NULL,
-  `contrasena` varchar(10) NOT NULL
+  `contrasena` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -72,7 +74,7 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id`, `user`, `contrasena`) VALUES
-(1, 'Alan', '1234');
+(1, 'miplaza', 'miplaza2023');
 
 --
 -- Índices para tablas volcadas
@@ -98,7 +100,7 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `card-nosotros`
 --
 ALTER TABLE `card-nosotros`
-  MODIFY `id` int(250) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(250) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
